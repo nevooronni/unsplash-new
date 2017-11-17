7a066d278b194ffc0756865354df2b90b027834a
Neville Oronni <nevooronni@gmail.com>
2017-11-17 18:11:09 +0300

    added tag modeld and table

--- unsplash/app/models.py
+++ unsplash/app/models.py
@@ -5,3 +5,19 @@ class Editor(models.Model):
 	last_name = models.CharField(max_length = 30)
 	email = models.EmailField()
 
+	# try:
+	# 	editor = Editor.objects.get(email = 'example@gamil.com')
+	# 	print('Editor found')
+	# except DoesNotExist:
+	# 	print('Editor was not found')
+
+	def __str__(self):#makes it easier to read our models in the shell return a string representation of our model 
+		return self.first_name
+	class Meta:
+		ordering = ['first_name']#specifying model specific options 
+
+class tags(models.Model):
+	name = models.CharField(max_length = 30)
+
+	def __str__(self):
+		return self.name 
