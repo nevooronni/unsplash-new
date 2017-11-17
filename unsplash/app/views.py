37616dd9efd9223acec3ee7745f086018f597164
Neville Oronni <nevooronni@gmail.com>
2017-11-17 12:14:38 +0300

    added fuction for specific dates and day of the week

--- unsplash/app/views.py
+++ unsplash/app/views.py
@@ -7,11 +7,24 @@ def welcome(request):
 
 def pics_of_day(request):
 	date = dt.date.today()
+
+	#function to find exact day 
+	day = convert_dates(date)
 	html = f'''
 				<html>
 						<body>
-								<h1> {date.day}-{date.month}-{date.year}</h1>
+								<h1>Pics for {day} {date.day}-{date.month}-{date.year}</h1>
 						</body>
 				</html>
 					'''
 	return HttpResponse(html)
+
+def convert_dates(dates):#takes in date object
+	#functions that gets the weekday number for the date
+	day_number = dt.date.weekday(dates)#call date.weekday pass in date object
+
+	days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+
+	#return the actual day of the week
+	day = days[day_number]
+	return day 
