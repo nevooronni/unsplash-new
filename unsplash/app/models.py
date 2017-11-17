188288fd975cfbe2cd4311fd679bba188d3ed609
Neville Oronni <nevooronni@gmail.com>
2017-11-17 17:33:33 +0300

    added editor model and created table

--- unsplash/app/models.py
+++ unsplash/app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Editor(models.Model):
+	first_name = models.CharField(max_length = 30)#string field for small to large sized strings passes a required argument max_length
+	last_name = models.CharField(max_length = 30)
+	email = models.EmailField()
+
