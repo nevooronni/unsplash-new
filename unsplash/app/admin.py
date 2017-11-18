44b202fdb9343950c14b71195d2092e15f248461
Neville Oronni <nevooronni@gmail.com>
2017-11-18 18:52:38 +0300

    added models and customization to our admin page

--- unsplash/app/admin.py
+++ unsplash/app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User,Photo,tags
 
-# Register your models here.
+class PhotoAdmin(admin.ModelAdmin):#inherits from the model admin class 
+	filter_horizontal = ('tags',)#allows ordering of many to many fields and pass in tags photo field
+
+admin.site.register(User)
+admin.site.register(Photo,PhotoAdmin)#pass class as second argument in fuction so new field will appear adjacent 
+admin.site.register(tags)
+
+
