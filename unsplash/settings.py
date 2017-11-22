16ac2c0bcaa1b2d8e3bc934735ffbe68a96ba4bb
Neville Oronni <nevooronni@gmail.com>
2017-11-22 14:05:34 +0300

    removed syntax error

--- unsplash/settings.py
+++ unsplash/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_dataxe_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 
